d4226abe5e30d8b401bfda30eac99f0ade74c56d
Mercy <mercycheruiyot2013@gmail.com>
2022-10-20 21:41:23 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    attr_accessor:name
+    attr_accessor:breed
+    def initialize (name="Sophie", breed="Chiwawa")
+        @name = name
+        @breed = breed
+    end
+    # def name=(name)
+    #     @name = name
+    #   end
+    #   def name
+    #     @name
+    #   end
 
 end
+
+Cuty= Dog.new("fiona","dog")
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-
+    attr_accessor :name
+    attr_accessor :job
+    def initialize(name ='BEYONCE', job="Singer")
+        @name = name
+        @job = job
+    end
+    
 end
+
+kanye = Person.new
+kanye.name=("Beyonce")
+kanye.job=("Singer")
